b05d1673a1903a37be02c1cb9529c8e52019a62d
Anurag Kumar <anurag.kumar@cartrack.com>
2026-06-30 23:41:10 +0530

    initialise template and layout

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def home():
-    return {"message":"ok"}
+posts: list[dict] = [
+	{
+		"id": 1,
+		"title": "First Post",
+		"content": "A test post whith some description",
+		"author": "Anurag",
+		"date_posted": "June 5th, 2026"
+	},
+	{
+		"id": 2,
+		"title": "Another Post",
+		"content": "A slightly different post from large body of ponds",
+		"author": "Amit",
+		"date_posted": "June 15th, 2026"
+	}
+
+]
+@app.get("/posts", include_in_schema=False)
+@app.get("/", include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
+
+
+@app.get("/api/posts")
+def get_posts():
+	return posts
